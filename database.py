a38ba8cf5d4f4bac0ed5761480a1e4ca3552af87
Shehryar-Malik-1108 <shehryarmalik1108@gmail.com>
2023-04-28 16:37:03 +0500

    first commit

---
+++ database.py
@@ -0,0 +1,69 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Bike(Base):
+    __tablename__ = "bikes"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    cc = Column(Integer, nullable=False)
+    color = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+
+
+class MyDatabase:
+    def __init__(self):
+        self.engine = create_engine("sqlite:///my_db.db")
+        Base.metadata.create_all(bind=self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+    def select_bike(self, id: int):
+        bike = self.session.query(Bike).filter_by(id=id).first()
+        if bike:
+            return {
+                "id": bike.id,
+                "name": bike.name,
+                "cc": bike.cc,
+                "color": bike.color,
+                "price": bike.price
+            }
+        else:
+            return None
+
+    def get_all_bikes(self):
+        bikes = self.session.query(Bike).all()
+        return {"bikes": [{"id": bike.id, "name": bike.name, "cc": bike.cc, "color": bike.color, "price": bike.price}
+                          for bike in bikes]}
+
+    def insert_bike(self, id: int, name: str, cc: int, color: str, price: int):
+        bike = Bike(id=id, name=name, cc=cc, color=color, price=price)
+        self.session.add(bike)
+        self.session.commit()
+        return f"Bike {name} created successfully"
+
+    def update_bike(self, id: int, price: int):
+        bike = self.session.query(Bike).filter_by(id=id).first()
+        if bike:
+            bike.price = price
+            self.session.commit()
+            return f"id {id} updated."
+        else:
+            return f"Error occurred while updating {id}."
+
+    def delete_bike(self, id: int):
+        bike = self.session.query(Bike).filter_by(id=id).first()
+        if bike:
+            self.session.delete(bike)
+            self.session.commit()
+            return f"id {id} deleted."
+        else:
+            return "Error occurred while deleting bike."
+
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from database import MyDatabase
+
+app = FastAPI(title="Sherry Ke Bikes")
+db = MyDatabase()
+
+
+@app.get("/")
+def home():
+    return "Welcome to Sherry kay Showroom!!"
+
+
+@app.get("/Select_Bike")
+def select_bike(id: int):
+    bike = db.select_bike(id)
+    if bike:
+        return bike
+    else:
+        return f"Bike: {id} does not exist!"
+
+
+@app.get("/All_Bikes")
+def get_all_bikes():
+    return db.get_all_bikes()
+
+
+@app.post("/Insert_bike")
+def insert_bike(id: int, name: str, cc: int, color: str, price: int):
+    result = db.insert_bike(id, name, cc, color, price)
+    return result
+
+
+@app.put("/Update_bike")
+def update_bike(id: int, price: int):
+    result = db.update_bike(id, price)
+    return result
+
+
+@app.delete("/Delete_bike")
+def delete_bike(id: int):
+    result = db.delete_bike(id)
+    return result
+
